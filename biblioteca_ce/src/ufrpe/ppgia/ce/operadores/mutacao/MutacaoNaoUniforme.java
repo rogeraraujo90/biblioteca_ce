45b6ce9d29b5f03dafd6a3e16c3d2fa70a9d8be6
gleidson.campos <gleidson.campos@ins-macbook-pro>
2017-04-29 11:15:07 -0300

    Algoritmo de mutação não uniforme

--- biblioteca_ce/src/ufrpe/ppgia/ce/operadores/mutacao/MutacaoNaoUniforme.java
+++ biblioteca_ce/src/ufrpe/ppgia/ce/operadores/mutacao/MutacaoNaoUniforme.java
@@ -1,14 +1,22 @@
 package ufrpe.ppgia.ce.operadores.mutacao;
 
+import java.util.Random;
+
 import ufrpe.ppgia.ce.base.OperadorMutacao;
 import ufrpe.ppgia.ce.base.solucao.SolucaoReal;
 
 public class MutacaoNaoUniforme implements OperadorMutacao<SolucaoReal> {
-
-	@Override
-	public SolucaoReal executarMutacao(SolucaoReal pai) {
-		// TODO Auto-generated method stub
-		return null;
+	public static int tamanhoFita = 8;
+	Random r = new Random();
+	
+	public float[] fita (float[]fita, float probabilidade){
+		for (int i =0; i<fita.length;i++){
+			float aux = r.nextFloat();
+			float aux2 = (float) r.nextGaussian();
+			if (aux<probabilidade){
+				fita[i] = (float) (fita[i] + aux2);
+			}
+		}
+		return fita;	
 	}
-
 }
